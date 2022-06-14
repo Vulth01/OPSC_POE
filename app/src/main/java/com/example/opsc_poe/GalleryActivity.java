cd98b3830e95272cb458473ab0669cc41140659d
Vulth01 <damienharwood14@gmail.com>
2022-06-14 19:28:14 +0200

    (test)

--- app/src/main/java/com/example/opsc_poe/GalleryActivity.java
+++ app/src/main/java/com/example/opsc_poe/GalleryActivity.java
@@ -24,53 +24,37 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-
         btn_Gallery = findViewById(R.id.btn_Gallery);
         btn_Settings = findViewById(R.id.btn_Settings);
         btn_Profile = findViewById(R.id.btn_Profile);
         btn_Home = findViewById(R.id.btn_Home);
         btn_Info = findViewById(R.id.btn_Info);
 
-        btn_Gallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(GalleryActivity.this, "btn_Gallery Pressed", Toast.LENGTH_SHORT).show();
-                Intent intentProfile = new Intent(GalleryActivity.this, GalleryActivity.class);
-                startActivity(intentProfile);
-            }
+        btn_Gallery.setOnClickListener(view -> {
+            Toast.makeText(GalleryActivity.this, "btn_Gallery Pressed", Toast.LENGTH_SHORT).show();
+            Intent intentProfile = new Intent(GalleryActivity.this, GalleryActivity.class);
+            startActivity(intentProfile);
         });
-        btn_Settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(GalleryActivity.this, "btn_Settings Pressed", Toast.LENGTH_SHORT).show();
-                Intent intentProfile = new Intent(GalleryActivity.this, SettingsActivity.class);
-                startActivity(intentProfile);
-            }
+        btn_Settings.setOnClickListener(view -> {
+            Toast.makeText(GalleryActivity.this, "btn_Settings Pressed", Toast.LENGTH_SHORT).show();
+            Intent intentProfile = new Intent(GalleryActivity.this, SettingsActivity.class);
+            startActivity(intentProfile);
         });
-        btn_Profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(GalleryActivity.this, "btn_Profile Pressed", Toast.LENGTH_SHORT).show();
-                Intent intentProfile = new Intent(GalleryActivity.this, ProfileActivity.class);
-                startActivity(intentProfile);
-            }
+        btn_Profile.setOnClickListener(view -> {
+            Toast.makeText(GalleryActivity.this, "btn_Profile Pressed", Toast.LENGTH_SHORT).show();
+            Intent intentProfile = new Intent(GalleryActivity.this, ProfileActivity.class);
+            startActivity(intentProfile);
         });
-        btn_Home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(GalleryActivity.this, "btn_Home Pressed", Toast.LENGTH_SHORT).show();
-                Intent intentProfile = new Intent(GalleryActivity.this, MainActivity.class);
-                startActivity(intentProfile);
-            }
+        btn_Home.setOnClickListener(view -> {
+            Toast.makeText(GalleryActivity.this, "btn_Home Pressed", Toast.LENGTH_SHORT).show();
+            Intent intentProfile = new Intent(GalleryActivity.this, MainActivity.class);
+            startActivity(intentProfile);
         });
 
-        btn_Info.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(GalleryActivity.this, "btn_Info Pressed", Toast.LENGTH_SHORT).show();
-                Intent intentProfile = new Intent(GalleryActivity.this, InfoActivity.class);
-                startActivity(intentProfile);
-            }
+        btn_Info.setOnClickListener(view -> {
+            Toast.makeText(GalleryActivity.this, "btn_Info Pressed", Toast.LENGTH_SHORT).show();
+            Intent intentProfile = new Intent(GalleryActivity.this, InfoActivity.class);
+            startActivity(intentProfile);
         });
 
         Log.d("One", "START");
